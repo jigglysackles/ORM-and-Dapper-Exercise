0f21affdd90e5ae12164c700d49960e3d036e671
John Hoffman <gnosisadvent@live.com>
2026-07-25 15:55:43 -0500

    created classes

---
+++ ORM-Dapper/IDepartmentRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace ORM_Dapper;
+
+public interface IDepartmentRepository
+{
+    
+}
--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -1,9 +1,25 @@
-﻿namespace ORM_Dapper
+﻿using System.Data;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+
+namespace ORM_Dapper
 {
     public class Program
     {
         static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string connString = config.GetConnectionString("DefaultConnection");
+
+            IDbConnection conn = new MySqlConnection(connString);
+            
+            
+            
+            
             Console.WriteLine("Hello, World!");
         }
     }
