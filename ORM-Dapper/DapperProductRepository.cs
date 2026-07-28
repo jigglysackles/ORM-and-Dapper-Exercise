a36f88b3312d1f7e3796c171198f6f25345a6f0f
John Hoffman <gnosisadvent@live.com>
2026-07-28 13:48:07 -0500

    Exercise 2 complete, wortking on bonus tasks

---
+++ ORM-Dapper/DapperProductRepository.cs
@@ -0,0 +1,25 @@
+﻿namespace ORM_Dapper;
+
+public class DapperProductRepository : IProductRepository
+{
+    public IEnumerable<Product> GetAllProducts()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void CreateProduct(string name, double price, int categoryID)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateProduct(string name, double price, int categoryID)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteProduct(int productID)
+    {
+        throw new NotImplementedException();
+    }
+    
+}
---
+++ ORM-Dapper/IProductRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace ORM_Dapper;
+
+public interface IProductRepository
+{
+    IEnumerable<Product> GetAllProducts();
+    
+    void CreateProduct(string name, double price, int categoryID);
+    
+}
